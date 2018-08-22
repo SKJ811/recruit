e910aeaf6d1f270beac97c118c806552a774ceac
SKJ811 <476212140@qq.com>
2018-08-22 16:25:47 +0800

    linsenter  success

--- recruit/src/main/java/com/yc/web/listeners/InitListener.java
+++ recruit/src/main/java/com/yc/web/listeners/InitListener.java
@@ -1,30 +1,35 @@
 package com.yc.web.listeners;
 
+import java.util.List;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-import javax.servlet.annotation.WebListener;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
-@WebListener
+import com.yc.bean.Job;
+import com.yc.biz.impl.JobBizImpl;
+
+@Component
 public class InitListener implements ServletContextListener {
 	private ApplicationContext ac;
-	public InitListener() {
-	}
+
 
 	public void contextDestroyed(ServletContextEvent arg0) {
 	}
 
 	public void contextInitialized(ServletContextEvent sce) {
-//		ServletContext application=sce.getServletContext();
-//		ac=WebApplicationContextUtils.getWebApplicationContext(application);
-//		JobBizImpl  jobBizImpl=(JobBizImpl) ac.getBean("JobBizImpl");
-//		//查出所有的行业 
-//		Job   job=new Job();
-//		List<Job>   industryList=jobBizImpl.getJob(job);
-//		application.setAttribute("industryList", industryList); 
-//		System.out.println(industryList);
-		System.out.println("1111111");
+		ServletContext application=sce.getServletContext();
+		ac=WebApplicationContextUtils.getWebApplicationContext(application);
+		JobBizImpl  jobBizImpl=(JobBizImpl) ac.getBean("jobBizImpl");
+		//查出所有的行业 
+		Job   job=new Job();
+		List<Job>   industryList=jobBizImpl.getJob(job);
+		application.setAttribute("industryList", industryList); 
+		System.out.println(industryList);
 	}
 
 }
